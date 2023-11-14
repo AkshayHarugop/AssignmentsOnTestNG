51be5ada81afb90857bdecf1d2b0050cc739d531
Akshay Harugop <99660258+ASHarugop@users.noreply.github.com>
2023-11-14 12:49:53 +0530

    Adding the day7.java class file for @dataprovider annotation, DataProvider.xml file added

---
+++ AssignmentsOnTestNG/src/test/day7.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class day7 {
+
+	@Test(dataProvider = "getData")
+	public void MobileSignoutCarLoan(String username, String password) {
+		System.out.println("mobileSignout");
+		System.out.println(username);
+		System.out.println(password);
+	}
+
+	@DataProvider
+	public Object[][] getData() {
+		// 1st combination - username and password -good credit history
+		// 2nd combination - username and password -no crdit history
+		// 3rd combination - username and password -fraudent credit history
+		Object[][] data = new Object[3][2];
+		data[0][0] = "firstSeleniumusername";
+		data[0][1] = "firstPassword";
+		data[1][0] = "SecondSeleniumusername";
+		data[1][1] = "SecondPassword";
+		data[2][0] = "ThirdSeleniumusername";
+		data[2][1] = "ThirdPassword";
+		return data;
+	}
+
+}

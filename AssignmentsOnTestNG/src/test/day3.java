257f316e6e7a4ea4b5b9bbc522e9748b8636e85b
Akshay Harugop <99660258+ASHarugop@users.noreply.github.com>
2023-11-14 11:51:32 +0530

    Updated the day3.java class file by adding timeout to a method

--- AssignmentsOnTestNG/src/test/day3.java
+++ AssignmentsOnTestNG/src/test/day3.java
@@ -4,17 +4,17 @@ import org.testng.annotations.Test;
 
 public class day3 {
 
-	@Test(groups= {"Smoke"})
+	@Test(groups = { "Smoke" })
 	public void WebloginCarLoan() {
 		System.out.println("weblogincar");
 	}
 
-	@Test(enabled=false)
+	@Test(enabled = false)
 	public void MobileloginCarLoan() {
 		System.out.println("mobilelogincar");
 	}
 
-	@Test
+	@Test(timeOut = 4000)
 	public void APIloginCarLoan() {
 		System.out.println("apilogincar");
 	}
